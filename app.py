10d89c77b4c6c3141aceaaf959b43d5c5456e20c
brar-karamjit <br.kramjit@gmail.com>
2026-03-01 14:32:49 -0500

    response

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, bro!\n'
+    return 'Hello Service online — cross-app call succeeded (Istio demo).'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
